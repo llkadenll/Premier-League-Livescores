ca7ee6b6204ebafd86af3de4062f5f600fb0f21f
llkadenll <kdwornik@o2.pl>
2022-09-06 17:05:42 +0200

    Minor refactor

--- src/App.js
+++ src/App.js
@@ -6,23 +6,9 @@ import scoresData from "./data.js"
 function App() {
   const [scores, setScores] = useState([])
   
-  // useEffect(() => {
-  //   fetch("../data.js")
-  //     .then(res => res.json())
-  //     .then(data => console.log(data))
-  // }, [])
-
   useEffect(() => {
     setScores(scoresData.events)
   }, [])
-
-  // function getScores() {
-    // const scoresArray = scoresData.events
-    // console.log(scoresArray)
-    // return 'elo'
-    // setScores(scoresData.events)
-    // console.log(scores)
-  // }
 
   return (
     <div className="app">
--- src/components/Scores.js
+++ src/components/Scores.js
@@ -2,11 +2,10 @@ import './Scores.css'
 import Match from "./Match"
 
 export default function Scores(props) {
+  // console.log(props.scores)
 
   const scores = props.scores.map(score => {
-    // console.log(score)
     return (
-      // JSON.stringify(score.awayTeam.name, score.homeTeam.name)
       <Match key={score.id} homeTeam={score.homeTeam.name} homeTeamSlug={score.homeTeam.slug} homeScore={score.homeScore.current} awayTeam={score.awayTeam.name} awayTeamSlug={score.awayTeam.slug} awayScore={score.awayScore.current} />
     )
   })
